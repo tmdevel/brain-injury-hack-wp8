ef9b2206c3d623ecb90ae655fdf4fc045ce6d1a2
tmdevel <tmdevel@gmail.com>
2014-05-12 17:41:37 +0100

    SupportService Intial checkin
    support service type and list control

--- MildBrainInjury/ServicesPage.xaml.cs
+++ MildBrainInjury/ServicesPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Device.Location;
+using System.Windows;
 using Microsoft.Phone.Controls;
 using Windows.Devices.Geolocation;
 
@@ -9,6 +11,33 @@ namespace MildBrainInjury
     public ServicesPage() {
       InitializeComponent();
       ShowMyLocationOnTheMap();
+
+
+      SetupDataForServices();
+    }
+
+    private void SetupDataForServices() {
+      /*
+      if (!App.ViewModel.IsDataLoaded) {
+        App.ViewModel.LoadData();
+      }
+      DataContext = App.ViewModel;*/
+      List<SupportSevice> source = new List<SupportSevice>();
+      source.Add(new SupportSevice("Carers NI", "www.carersni.org", "02890439843", 54.597, 5.93));
+      source.Add(new SupportSevice("Child Brain Injury", "www.cbituk.org", "02890817145", 54.597, 5.93));
+      source.Add(new SupportSevice("Cedar", "www.cedar-foundatiom.org", "02890666188", 54.597, 5.93));
+      source.Add(new SupportSevice("Jigsaw", "www.jigsawni.org.uk", "02890319054", 54.597, 5.93));
+      source.Add(new SupportSevice("Praxis Care", "www.praxisprovides.com", "02890234555", 54.597, 5.93));
+
+
+      List<AlphaKeyGroup<SupportSevice>> DataSource = AlphaKeyGroup<SupportSevice>.CreateGroups(source,
+                                                                                            System.Threading.Thread
+                                                                                                  .CurrentThread
+                                                                                                  .CurrentUICulture,
+                                                                                            (SupportSevice s) => { return s.Name; }, true);
+
+      ServicesList.ItemsSource = DataSource;
+
     }
 
     private async void ShowMyLocationOnTheMap() {
@@ -23,6 +52,10 @@ namespace MildBrainInjury
       //ServicesMap.Center = myGeoCoordinate;
       ServicesMap.Center = new GeoCoordinate(54.6077, -005.9194);
       ServicesMap.ZoomLevel = 15;
+    }
+
+    private void Pivot_Loaded(object sender, RoutedEventArgs e) {
+       
     }
   }
 }
