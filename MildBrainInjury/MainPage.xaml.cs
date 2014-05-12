5c4fa66231210ff2db9b1358b9d7a24f29d9593c
tmdevel <tmdevel@gmail.com>
2014-05-12 14:41:01 +0100

    Added Content
    Filled out the rest of the main views with content

--- MildBrainInjury/MainPage.xaml.cs
+++ MildBrainInjury/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
